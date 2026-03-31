4e22880320cbfde44e20a3c3ee780ffda85ef8f4
Eduardo Ventz Xavier <fullgamercsgo@hotmail.com>
2026-03-31 16:53:27 -0300

    Add Main class with Bola object demonstration

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args){
+
+        Bola bola1 = new Bola("Amarela", 3.5, "Pedra");
+        bola1.mostraCor();
+        bola1.trocarCor();
+        bola1.mostraCor();
+    }
+}
